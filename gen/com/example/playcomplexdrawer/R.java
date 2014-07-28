df736ad04faa6e527063b70110fa1503210dd5fb
Eric Liu <eric.liu.developer@gmail.com>
2014-07-28 17:04:56 +0800

    add refresh button

--- gen/com/example/playcomplexdrawer/R.java
+++ gen/com/example/playcomplexdrawer/R.java
@@ -27,14 +27,16 @@ public final class R {
     public static final class drawable {
         public static final int drawer_shadow=0x7f020000;
         public static final int ic_action_create=0x7f020001;
-        public static final int ic_action_settings=0x7f020002;
-        public static final int ic_drawer=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int tklogo=0x7f020005;
+        public static final int ic_action_refresh=0x7f020002;
+        public static final int ic_action_settings=0x7f020003;
+        public static final int ic_drawer=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int tklogo=0x7f020006;
     }
     public static final class id {
-        public static final int action_create=0x7f090008;
-        public static final int action_settings=0x7f090007;
+        public static final int action_create=0x7f090009;
+        public static final int action_refresh=0x7f090007;
+        public static final int action_settings=0x7f090008;
         public static final int content_frame=0x7f090002;
         public static final int drawerListLayout=0x7f090003;
         public static final int drawer_layout=0x7f090001;
@@ -49,6 +51,7 @@ public final class R {
         public static final int activity_navi_drawer=0x7f030002;
         public static final int drawer_row=0x7f030003;
         public static final int fragment_tab=0x7f030004;
+        public static final int progressbar=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/com/example/playcomplexdrawer/HomeActivity.java
+++ src/com/example/playcomplexdrawer/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
 import android.app.FragmentTransaction;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,8 +20,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class HomeActivity extends ActionTabActivity implements TabListener {
-	ViewPager mPager ;
-	
+	ViewPager mPager;
+	MenuItem menuItem;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +33,33 @@ public class HomeActivity extends ActionTabActivity implements TabListener {
 		mPager = (ViewPager) layout.findViewById(R.id.pager);
 		MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
 		mPager.setAdapter(adapter);
-		
-		
+
 		final ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		for (int i = 0; i < adapter.getCount(); i++) {
-			actionBar.addTab(actionBar.newTab().setText("Tab " + i).setTabListener(this));
-			
+			actionBar.addTab(actionBar.newTab().setText("Tab " + i)
+					.setTabListener(this));
+
 		}
-		
+
 		mPager.setOnPageChangeListener(new OnPageChangeListener() {
-			
+
 			@Override
 			public void onPageSelected(int position) {
 				// TODO Auto-generated method stub
 				actionBar.setSelectedNavigationItem(position);
 			}
-			
+
 			@Override
 			public void onPageScrolled(int arg0, float arg1, int arg2) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void onPageScrollStateChanged(int arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 	}
@@ -69,15 +70,23 @@ public class HomeActivity extends ActionTabActivity implements TabListener {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+
+		switch (id) {
+		case R.id.action_refresh:
+
+			menuItem = item;
+			menuItem.setActionView(R.layout.progressbar);
+			new UpdateTask().execute();
+			break;
+
+		default:
+			break;
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
-	
-	private static class MyPagerAdapter extends FragmentPagerAdapter{
+
+	private static class MyPagerAdapter extends FragmentPagerAdapter {
 
 		public MyPagerAdapter(FragmentManager fm) {
 			super(fm);
@@ -88,7 +97,7 @@ public class HomeActivity extends ActionTabActivity implements TabListener {
 		public Fragment getItem(int position) {
 			// TODO Auto-generated method stub
 			Fragment frag = TabFragment.newInstance(position);
-			
+
 			return frag;
 		}
 
@@ -97,43 +106,41 @@ public class HomeActivity extends ActionTabActivity implements TabListener {
 			// TODO Auto-generated method stub
 			return 3;
 		}
-		
-		
+
 	}
-	
-	
+
 	public static class TabFragment extends Fragment {
 		TextView tvMain;
-		
+
 		public TabFragment() {
-			
+
 		}
-		
-		public static TabFragment newInstance(int position){
+
+		public static TabFragment newInstance(int position) {
 			TabFragment frag = new TabFragment();
 			Bundle args = new Bundle();
 			args.putInt("position", position);
 			frag.setArguments(args);
 			return frag;
 		}
-		
+
 		@Override
 		public View onCreateView(LayoutInflater inflater,
 				@Nullable ViewGroup container,
 				@Nullable Bundle savedInstanceState) {
 			// TODO Auto-generated method stub
-			View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
+			View rootView = inflater.inflate(R.layout.fragment_tab, container,
+					false);
 			tvMain = (TextView) rootView.findViewById(R.id.tvMain);
-			String text =String.valueOf(getArguments().getInt("position")) ;
+			String text = String.valueOf(getArguments().getInt("position"));
 			if (text != null) {
-				
+
 				tvMain.setText(text);
 			}
-			
+
 			return rootView;
 		}
 	}
-
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
@@ -144,12 +151,37 @@ public class HomeActivity extends ActionTabActivity implements TabListener {
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
+
+	}
+
+	private class UpdateTask extends AsyncTask<Void, Integer, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
 		
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			
+			menuItem.setActionView(null);
+		}
 	}
 }
